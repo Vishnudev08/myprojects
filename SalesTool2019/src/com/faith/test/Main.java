c22e8772a91071eaec1718939d928575ee85f869
dev <vishnudevkp98@gmail.com>
2019-10-04 10:27:45 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,20 @@
+
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesDate objeSalesData = new SalesDate();
+		System.out.println("hello UST Guys ! Git Repo");
+		displatGreeting();
+		objeSalesData.display();
+	}
+
+	private static void displatGreeting() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to Sales Report App 2019...");
+	}
+
+}
---
+++ SalesTool2019/src/com/faith/test/SalesDate.java
@@ -0,0 +1,8 @@
+package com.faith.test;
+
+public class SalesDate {
+
+	void display(){
+		System.out.println("Sales Report");
+	}
+}
